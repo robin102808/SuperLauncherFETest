71242e2fa5a63eb4474dab62ed62033d52eecb91
Robin <robin102808@gmail.com>
2022-03-14 21:06:06 -0400

    Update .eslintrc.js

--- front-end-typescript/.eslintrc.js
+++ front-end-typescript/.eslintrc.js
@@ -7,15 +7,15 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    'plugin:prettier/recommended'
+    // 'plugin:prettier/recommended'
   ],
-  'plugins': ['prettier'],
+  // 'plugins': ['prettier'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': 'error'
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'prettier/prettier': 0
   }
 }
